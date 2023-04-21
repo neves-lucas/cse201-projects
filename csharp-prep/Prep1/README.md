e32363c4a030c4e7028be59440a897f6a87b7e70
neves-lucas <neves.lucas.r@gmail.com>
2023-04-21 10:38:51 -0300

    Prep 2 - Conditionals + Readme

---
+++ csharp-prep/Prep1/README.md
@@ -0,0 +1,14 @@
+Assignment
+Prompt the user for their first name. Then, prompt them for their last name. Display the text back all on one line saying, "Your name is last-name, first-name, last-name" as shown:
+
+
+What is your first name? Scott
+What is your last name? Burton
+
+Your name is Burton, Scott Burton.
+
+What is your first name? Brigham
+What is your last name? Young
+
+Your name is Young, Brigham Young.
+Make sure to be precise! You should have the spacing, comma, and period appear exactly as shown in the examples.
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int grade = int.Parse(input);
+        string letter = "";
+        string sign = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else {
+            letter = "F";
+        }
+
+        if (grade >= 70)
+        {
+            if (grade % 10 >= 7)
+            {
+                sign = "+";
+                if (grade >= 90 || grade < 60)
+                {
+                    sign = "";
+                }
+            }
+            else {
+                sign = "-";
+            }
+
+            Console.WriteLine("Congratulations! You've passed! ");
+            Console.WriteLine($"{letter}{sign}");
+        }
+        else {
+            Console.WriteLine($"{letter}{sign}");
+            Console.WriteLine("Sorry, you've not passed. Try again the next time. ");
+        }
     }
 }
