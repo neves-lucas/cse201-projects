294dca17884126cc3a2c48702b0c9ae8766a3ba8
neves-lucas <neves.lucas.r@gmail.com>
2023-04-22 18:23:43 -0300

    C# - Prep 3 - Loops + README

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+        string loop = "true";
+        int guesses = 0;
+
+        while (loop == "true")
+        {    
+            Console.WriteLine("What is your guess? ");
+            string input = Console.ReadLine();
+            int numberGuess = int.Parse(input);
+
+            if (numberGuess < magicNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Higher. ");
+                guesses += 1;
+            }
+            else if (numberGuess > magicNumber)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Lower. ");
+                guesses += 1;
+            }
+            else
+            {
+                Console.WriteLine("You guessed it! ");
+                Console.WriteLine();
+                Console.WriteLine($"You tried {guesses} times. ");
+                Console.WriteLine("You want to play again? [y/n] ");
+                string response = Console.ReadLine();
+                if (response == "y")
+                    {
+                        Console.WriteLine();
+                        loop = "true";
+                        magicNumber = randomGenerator.Next(1, 100);
+                    }
+                else if (response == "n")
+                    {
+                        Console.WriteLine();
+                        Console.WriteLine("Thank you for playing. See you at the next time :)");
+                        loop = "false";
+                    }
+                else
+                    {
+                        Console.WriteLine("Wrong input, start the program again.");
+                        loop = "false";
+                    }
+            }
+        
+        }
     }
 }
---
+++ csharp-prep/Prep3/README.md
@@ -0,0 +1,15 @@
+1. Start by asking the user for the magic number. (In future steps, you will change this to have the computer generate a random number, but to get started, we'll just let the user decide what it is.)
+Ask the user for a guess.
+Using an if statement, determine if the user needs to guess higher or lower next time, or tell them if they guessed it.
+At this point, you won't have any loops.
+
+2. Add a loop that keeps looping as long as the guess does not match the magic number.
+At this point, the user should be able to keep playing until they get the correct answer.
+
+3. Instead of having the user supply the magic number, generate a random number from 1 to 100.
+Play the game and make sure it works!
+
+Stretch Challenge:
+
+1. Keep track of how many guesses the user has made and inform them of it at the end of the game.
+2. After the game is over, ask the user if they want to play again. Then, loop back and play the whole game again and continue this loop as long as they keep saying "yes".
