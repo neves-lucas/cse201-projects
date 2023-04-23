ceca7ddc211e07e8fd141284790248d1748093b4
neves-lucas <neves.lucas.r@gmail.com>
2023-04-23 16:08:44 -0300

    C# Prep 5 - Functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+
+        string name = PromptUserName();
+        int number = PromptUserNumber();
+
+        int squaredNumber = SquareNumber(number);
+
+        DisplayResult(name, squaredNumber);
     }
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program! ");
+        }
+
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            return number;
+        }
+
+        static int SquareNumber(int number)
+        {
+            int squaredNumber = number * number;
+            return squaredNumber;
+        }
+
+        static void DisplayResult(string name, int squaredNumber)
+        {
+            Console.WriteLine($"{name}, the square of your number is {squaredNumber}");
+        }
 }
---
+++ csharp-prep/Prep5/README.md
@@ -0,0 +1,18 @@
+# **Assignment Instructions**
+
+For this assignment, write a C# program that has several simple functions:
+
+**DisplayWelcome** - Displays the message, "Welcome to the Program!"
+**PromptUserName** - Asks for and returns the user's name (as a string)
+**PromptUserNumber** - Asks for and returns the user's favorite number (as an integer)
+**SquareNumber** - Accepts an integer as a parameter and returns that number squared (as an integer)
+**DisplayResult** - Accepts the user's name and the squared number and displays them.
+
+Your Main function should then call each of these functions saving the return values and passing data to them as necessary.
+
+Sample output of the program could look as follows:
+
+_Welcome to the program!_
+_Please enter your name: Brother Burton_
+_Please enter your favorite number: 42_
+_Brother Burton, the square of your number is 1764_
