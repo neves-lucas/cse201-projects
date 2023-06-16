e4e0967108fa45ba273d3d20c7b2b9d4525653c9
neves-lucas <neves.lucas.r@gmail.com>
2023-06-16 20:03:58 -0300

    Shapes program done

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -17,47 +17,47 @@ namespace Journal
         }
 
         // The AddEntry method adds a new entry to this journal's list of entries.
- public void AddEntry(Entry entry)
- {
- _entries.Add(entry);
- }
+        public void AddEntry(Entry entry)
+        {
+            _entries.Add(entry);
+        }
 
- // The DisplayEntries method displays all of this journal's entries to the console.
- public void DisplayEntries()
- {
- foreach (Entry entry in _entries)
- {
- Console.WriteLine(entry);
- }
- }
+        // The DisplayEntries method displays all of this journal's entries to the console.
+        public void DisplayEntries()
+        {
+            foreach (Entry entry in _entries)
+            {
+                Console.WriteLine(entry);
+            }
+        }
 
- // The LoadEntries method clears this journal's list of entries and loads any existing entries from the specified file into this list.
- public void LoadEntries(string fileName)
- {
- _entries.Clear();
- if (File.Exists(fileName))
- {
- string[] lines = File.ReadAllLines(fileName);
- foreach (string line in lines)
- {
- string[] parts = line.Split(new[] { ": ", " - " }, StringSplitOptions.None);
- DateTime date = DateTime.Parse(parts[0]);
- string prompt = parts[1];
- string text = parts[2];
- _entries.Add(new Entry(text, prompt) { Date = date });
- }
- }
- }
+        // The LoadEntries method clears this journal's list of entries and loads any existing entries from the specified file into this list.
+        public void LoadEntries(string fileName)
+        {
+            _entries.Clear();
+            if (File.Exists(fileName))
+            {
+                string[] lines = File.ReadAllLines(fileName);
+                foreach (string line in lines)
+                {
+                    string[] parts = line.Split(new[] { ": ", " - " }, StringSplitOptions.None);
+                    DateTime date = DateTime.Parse(parts[0]);
+                    string prompt = parts[1];
+                    string text = parts[2];
+                    _entries.Add(new Entry(text, prompt) { Date = date });
+                }
+            }
+        }
 
- // The SaveEntries method saves all of this journal's entries to the specified file. Each entry is saved on its own line using its ToString representation. If any existing file with this name is overwritten. 
- public void SaveEntries(string fileName)
- {
- List<string> lines = new List<string>();
- foreach (Entry entry in _entries)
- {
- lines.Add(entry.ToString());
- }
- File.WriteAllLines(fileName, lines);
- }
- }
+        // The SaveEntries method saves all of this journal's entries to the specified file. Each entry is saved on its own line using its ToString representation. If any existing file with this name is overwritten. 
+        public void SaveEntries(string fileName)
+        {
+            List<string> lines = new List<string>();
+            foreach (Entry entry in _entries)
+            {
+                lines.Add(entry.ToString());
+            }
+            File.WriteAllLines(fileName, lines);
+        }
+    }
 }
